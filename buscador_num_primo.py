9d72f79708f0503e5605a8d017b96897dce65dd9
Alex Quispe Torres <alexqtorre@gmail.com>
2022-07-29 17:26:58 -0500

    modifica la descripcion del numero primo

--- buscador_num_primo.py
+++ buscador_num_primo.py
@@ -2,7 +2,7 @@ def run(valor):
     for i in range(2,valor+1):     # Ciclo incrementa i : Dividendo 
         for j in range(2,valor+1): # Ciclo incrementa j; Divisor             
             if j >= i:                                                          # Si el Divisor llega a ser mayor o igual que el dividendo.
-                print(i, '   Es numero primo ' )                                # El numero es primo y se interrumpe el ciclo. 
+                print(i, '   Es un numero primo ' )                                # El numero es primo y se interrumpe el ciclo. 
                 break                            
             elif i>j and i%j==0 and j>1:                                        # Si el dividendo es mayor que el divisor, el divisor es mayor que 1 y la division es exacta.
                 break                                                           # El numero no es primo y se interrumpe el ciclo.
