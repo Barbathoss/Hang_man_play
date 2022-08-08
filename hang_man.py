612fd2bd0219fed93bcd5613c6bac61ada030067
Alex Quispe Torres <alexqtorre@gmail.com>
2022-08-07 19:35:42 -0500

    Se agrega ascii art

--- hang_man.py
+++ hang_man.py
@@ -1,4 +1,63 @@
 import random
+import os
+
+PICS = ["""
+   +--+
+   |  |
+      |
+      |
+      |
+      |
+  =====""",
+ """
+   +--+
+   |  |
+   O  |
+      |
+      |
+      |
+  =====""",
+ """
+   +--+
+   |  |
+   O  |
+   |  |
+      |
+      |
+  =====""",
+ """
+   +--+
+   |  |
+   O  |
+  /|  |
+      |
+      |
+  =====""",
+ """
+   +--+
+   |  |
+   O  |
+  /|\ |
+      |
+      |
+  =====""",
+ """
+   +--+
+   |  |
+   O  |
+  /|\ |
+  /   |
+      |
+=====""",
+ """
+   +--+
+   |  |
+   O  |
+  /|\ |
+  / \ |
+      |
+  ====="""]
+
 
 def read_data():
     word_data = []
@@ -21,10 +80,8 @@ def hiding_word(h):
     word_hide=h
     mask=""
     for i in word_hide:
-        mask+="_"
-    print("""           
-    
-    """)    
+        mask+="_" 
+    print("   ")    
     print(mask)
     return (mask)
 
@@ -42,7 +99,7 @@ def check_word(m,k,letter_in):
         else:
             count+=1                                                 
     mask_word="".join(mask_word)
-    print(mask_word)
+    print(mask_word.upper())
     return mask_word       
 
 
@@ -51,26 +108,31 @@ def run():
     word_win=read_data()
     word_win=normalize(word_win)
     word_mask=hiding_word(word_win)
+    global lose
     lose=0
     resul=word_mask
     win=False
     while lose<7 or win == False:
-        
         letter=input("Ingrese una letra por favor  ")
         
         if letter in word_win:
+            os.system("clear")
             visto =check_word(word_win,resul,letter)
             resul=visto
             if resul in word_win:
                 win=True
+                os.system("clear")
+                print(resul.upper())
                 print("GANASTE !!!!!!!")
                 break
             
         else:
             lose +=1
-            print("Te quedan ",7-lose," intentos")
-            print("Has perdido",lose," ❤ menos")
-            if lose >= 7:
+            os.system("clear")
+            print("Te queda ",7-lose," ❤")
+            print(resul.upper())
+            print(PICS[lose]) 
+            if lose >= 6:
                 print("PERDISTE!!!!!!!!")
                 break 
         
